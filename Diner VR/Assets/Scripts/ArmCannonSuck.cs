73eab9182f2fab67114ed9a656600689df612554
Tobey-Julian Joseph <tjoseph5@student.fitchburgstate.edu>
2022-11-13 19:08:14 -0500

    updates

--- Diner VR/Assets/Scripts/ArmCannonSuck.cs
+++ Diner VR/Assets/Scripts/ArmCannonSuck.cs
@@ -9,16 +9,28 @@ public class ArmCannonSuck : MonoBehaviour
     public InputActionReference trigger;
     public InputActionReference grip;
 
+    GameObject suckZone;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        suckZone = this.transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
+        int triggerInt = trigger.action.ReadValue<int>();
+        int gripInt = grip.action.ReadValue<int>();
         
+        if(gripInt == 0 && triggerInt == 0)
+        {
+            suckZone.SetActive(false);
+        }
+        else if(gripInt == 1 && triggerInt == 1)
+        {
+            suckZone.SetActive(true);
+        }
     }
 
     private void OnEnable()
