954887697d6180c2c156041437f136ad64a96a9f
tjoseph5 <tjoseph5@student.fitchburgstate.edu>
2022-11-21 14:01:46 -0500

    ahhh

---
+++ Diner VR/Assets/Scripts/ItemPool.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item Spawning Pool")]
+public class ItemPool : ScriptableObject
+{
+    public List<GameObject> items;
+}
