4855b2c386dc8607a3bc769b9f68e0f95af3b2cc
tjoseph5 <tjoseph5@student.fitchburgstate.edu>
2022-11-28 21:57:54 -0500

    ahhh

--- Diner VR/Assets/Scripts/EnemySpawning.cs
+++ Diner VR/Assets/Scripts/EnemySpawning.cs
@@ -1,11 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class EnemySpawning : MonoBehaviour
 {
     public ItemPool originalPool;
-    public GameObject[] enemyPool;
 
     public GameObject currentEnemy;
 
@@ -23,18 +23,17 @@ public class EnemySpawning : MonoBehaviour
     {
         currentSpawnTime = 0;
 
-        itemRandomID = Random.Range(minimumIndexID, enemyPool.Length);
+        itemRandomID = GiveMeANumber();
 
-        enemyPool = originalPool.items;
+        currentEnemy = originalPool.items[itemRandomID];
 
-        currentEnemy = enemyPool[itemRandomID];
+
     }
 
     private void Update()
     {
         if (canRefresh)
         {
-            IndexUpdater();
             currentSpawnTime += Time.deltaTime;
 
             if(currentSpawnTime >= currentSpawnDuration)
@@ -46,14 +45,20 @@ public class EnemySpawning : MonoBehaviour
         {
             currentSpawnTime = 0;
         }
+
+        if (debugCSA_Trigger)
+        {
+            itemRandomID = GiveMeANumber();
+            debugCSA_Trigger = false;
+        }
     }
 
     public void SpawnEnemies()
     {
         currentSpawnTime = 0;
 
-        itemRandomID = Random.Range(minimumIndexID, enemyPool.Length);
-        currentEnemy = enemyPool[itemRandomID];
+        itemRandomID = GiveMeANumber();
+        currentEnemy = originalPool.items[itemRandomID];
 
         GameObject spawnedEnemy = Instantiate(currentEnemy, transform.position, transform.rotation, null);
 
@@ -76,28 +81,30 @@ public class EnemySpawning : MonoBehaviour
         }
     }
 
-    public void IndexUpdater()
+    private int GiveMeANumber()
     {
+        var exclude = new HashSet<int>();
+        var range = Enumerable.Range(0, originalPool.items.Count()).Where(i => !exclude.Contains(i));
+
+        var rand = new System.Random();
+        int index = rand.Next(0, originalPool.items.Count() - exclude.Count);
+
+
         if(EnemyManager.instance.waffleEnemyAmt == 0)
         {
-            minimumIndexID = 1;
+            exclude.Add(0);
         }
 
-        if(EnemyManager.instance.waffleEnemyAmt == 0 && EnemyManager.instance.friesEnemyAmt == 0)
+        if (EnemyManager.instance.friesEnemyAmt == 0)
         {
-            minimumIndexID = 2;
+            exclude.Add(1);
         }
 
-        if (EnemyManager.instance.waffleEnemyAmt == 0 && EnemyManager.instance.friesEnemyAmt == 0 && EnemyManager.instance.waffleEnemyAmt == 0)
+        if (EnemyManager.instance.milkshakeEnemyAmt == 0)
         {
-            canRefresh = false;
+            exclude.Add(2);
         }
 
-        if(EnemyManager.instance.waffleEnemyAmt >= 1 && EnemyManager.instance.friesEnemyAmt >= 1 && EnemyManager.instance.waffleEnemyAmt >= 1)
-        {
-            canRefresh = true;
-
-            minimumIndexID = 0;
-        }
+        return range.ElementAt(index);
     }
 }
